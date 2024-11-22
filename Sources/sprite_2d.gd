6a29d7600a9644ed489f9287a9e5f6349435edad
yabby1997 <yabby1997@gmail.com>
2024-11-22 20:25:25 +0900

    Change to move as user input

--- Sources/sprite_2d.gd
+++ Sources/sprite_2d.gd
@@ -8,5 +8,17 @@ func _init():
 
 func _process(delta: float) -> void:
 	print(delta)
-	rotation += angular_speed * delta
-	position += Vector2.UP.rotated(rotation) * speed * delta
+	
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+		
+	rotation += angular_speed * direction * delta
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+		
+	position += velocity * delta
